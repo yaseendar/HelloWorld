57784f43141a9d2cb2181ac6feef105b9972ce83
yaseendar <yaseen@trialx.com>
2016-02-02 23:02:50 +0530

    register model under django admin and made added and served static files for templates

--- names/admin.py
+++ names/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import Name
+
 
 # Register your models here.
+
+class NameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'saved_on')
+    order_by = ('saved_on',)
+
+
+admin.site.register(Name, NameAdmin)
