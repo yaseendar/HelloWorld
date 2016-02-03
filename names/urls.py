149734d3791c23965d1b460bc41ede86728f0577
yaseendar <yaseen@trialx.com>
2016-02-03 11:06:12 +0530

    generate dummy token call added

--- names/urls.py
+++ names/urls.py
@@ -3,8 +3,12 @@ __author__ = 'yaseen'
 from django.conf.urls import url
 from views import AddNameView
 from views import DisplayName
+from views import GenerateDummyToken
+from views import AllNames
 
 urlpatterns = [
     url(r'^$', AddNameView.as_view(), name='name-add'),
     url(r'display/$', DisplayName.as_view(), name='display-name'),
+    url(r'api/display/allnames/$', AllNames.as_view(), name='display-allnames'),
+    url(r'api/dummytoken/$', GenerateDummyToken.as_view(), name='dummy-token'),
 ]
--- names/views.py
+++ names/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+
 from forms import NameForm
 from models import Name
 
@@ -21,4 +28,25 @@ class DisplayName(TemplateView):
         name_obj = Name.objects.latest('id')
         return self.render_to_response(dict(latest_name=name_obj))
 
+
+class GenerateDummyToken(APIView):
+    def get(self, request):
+        user, created = User.objects.get_or_create(username='test',
+                                                   email='test@test.com',
+                                                   password='password123')
+        try:
+            token = Token.objects.get(user=user)
+        except:
+            token = Token.objects.create(user=user)
+
+        return Response(token.key)
+
+
+class AllNames(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        names = Name.objects.all().values('name', 'saved_on')
+        return Response(names)
 
